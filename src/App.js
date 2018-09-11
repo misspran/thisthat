3b5b4455513cc50f74e4700e40ac8fd1b998a136
Vi Tran <vipran@gmail.com>
2018-09-11 17:06:28 -0400

    changes to json

--- src/App.js
+++ src/App.js
@@ -6,19 +6,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
- 
+  componentDidMount =() => this.props.fetchthisthat()
   handleButtonClick = () => this.props.fetchthisthat()
   render() {
     
     console.log(this.props)
     return (
       <Container>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Let me pitch you a startup idea.</h1>
-        </header>
+        <h1>Let me pitch you this startup idea.</h1>
         <p className="App-intro">
-          It's like {this.props.thisthat.this} for {this.props.thisthat.that}.
+          What about {this.props.thisthat.this} for {this.props.thisthat.that}.
         </p>
         <Button onClick={this.handleButtonClick}  color='green'>Refresh</Button>
       </Container>
