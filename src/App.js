39df30cf2dcbdfad0d0126bc5ec16fd9eb34d22c
Vi Tran <vipran@gmail.com>
2018-09-11 21:02:07 -0400

    add social share button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {fetchthisthat} from './store'
-import { Button, Container, Grid, Image } from 'semantic-ui-react'
-import logo from './logo.svg';
+import { 
+  Button, 
+  Container, 
+  Grid, 
+  Image 
+} from 'semantic-ui-react'
+import {
+  FacebookShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  RedditShareButton,
+  EmailShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  LinkedinIcon,
+  RedditIcon,
+  EmailIcon
+} from 'react-share';
 import './App.css';
 
 class App extends Component {
@@ -22,6 +38,20 @@ class App extends Component {
           How about <span id="this">{this.props.thisthat.this}</span> for <span id="that">{this.props.thisthat.that}</span>.
         </p>
         <Button onClick={this.handleButtonClick}  color='magenta' size='massive'>Refresh</Button>
+        <div className="social-share">
+        <FacebookShareButton url={"http://thisthatapp.surge.sh"} className="share-button">
+          <FacebookIcon size={42} quote={'Let me pitch you this startup idea...'} round />
+        </FacebookShareButton>
+        <TwitterShareButton url={"http://thisthatapp.surge.sh"} className="share-button" title={'Let me pitch you this startup idea...'}>
+          <TwitterIcon size={42} round />
+        </TwitterShareButton>
+        <RedditShareButton url={"http://thisthatapp.surge.sh"} className="share-button" title={'Let me pitch you this startup idea...'}>
+          <RedditIcon size={42} round />
+        </RedditShareButton>
+        <LinkedinShareButton url={"http://thisthatapp.surge.sh"} className="share-button" title={'Let me pitch you this startup idea...'}>
+          <LinkedinIcon size={42} round />
+        </LinkedinShareButton>
+        </div>
         </Grid.Column>
         <Grid.Column floated="right" padded='vertically'>
         <Image src="https://i.redd.it/cysuznbvc0py.png" id="giant-head" floated='right' stretched/>
